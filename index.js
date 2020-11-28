23a49a3273e07c51569ef47cc291466e0e0b9c0c
Binay bk <khadkabinay@yahoo.com>
2020-11-28 14:07:36 -0600

    Adds insertionSort2 solution

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-// coding challenges 
+// INSERTION SORT PART2
 
-console.log("coding challenges")
+function insertionSort2(arr) {
+    for(let i = 1; i < arr.length; i++){
+        let temp = arr[i]
+        let j = i - 1
+        while(arr[j] > temp){
+            arr[j + 1] = arr[j]
+            j--
+        }
+        //increements j because it's value was less becuase of while loop j--
+        j++
+        arr[j] = temp
+    }
+return arr
+}
+
+insertionSort2([1,3,10, 35, 56, 78])

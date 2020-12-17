4baf0d724276fa0c33bda7f5246968376e63d738
Binay bk <khadkabinay@yahoo.com>
2020-12-17 11:41:37 -0600

    Adds solutions for merge tree

--- index.js
+++ index.js
@@ -280,6 +280,33 @@ isSubsequence('aceee', 'abcde')
 //BINARY TREES 
 //1.Merge Two Binary Trees
 
+function TreeNode(val, left, right){
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+
+
+function mergeTrees(t1, t2) {
+        if(!t1){
+            return t2
+        }
+        if(!t2){
+        return t1
+        }
+
+        t1.val += t2.val
+        t1.left = mergeTrees(t1.left, t2.left)
+        t1.right = mergeTrees(t1.right, t2.right)
+
+        return t1
+        
+    
+}
+
+
+
+
 
 //2.same tree
 // Given two binary trees, write a function to check if they are the same or not.

3fabb9ca15985af4796ac706fdcb11059d1b952f
Binay bk <khadkabinay@yahoo.com>
2020-12-21 10:55:43 -0600

    Adds interpret solution

--- index.js
+++ index.js
@@ -313,6 +313,46 @@ const  runningSum = function(nums) {
     
 }
 
+//12.Goal Parser Interpretation 
+//You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+
+const interpret = function(command) {
+    //define a variable that holds result
+    //iterate over string
+    //check if current value and next value matched with "()" , if Yes, replace with o
+    //otherwise ,add rest of string to the result
+    // returns that result
+    
+    let result = ''
+    for(let i = 0; i < command.length; i++){
+        let firstChar  = command.charAt(i)
+        let nextChar   = command.charAt(i + 1)
+        
+        if(firstChar ===  "(" && nextChar  === ")" ){
+            result += 'o'
+            i += 1
+        }else{
+            if(firstChar != "(" && firstChar != ")" ){
+            result += firstChar
+            }
+        }
+        
+        
+    }
+    
+    return result
+    
+
+};
+
+interpret('G()()(al)')
+
+
+
+
+
+
 
 
 

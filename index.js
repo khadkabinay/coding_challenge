f6da4bc85fcd096cbbf0b5885492829af09b454a
Binay bk <khadkabinay@yahoo.com>
2020-12-22 23:22:38 -0600

    Removes psuedocode for binary tree

--- index.js
+++ index.js
@@ -351,20 +351,3 @@ interpret('G()()(al)')
 
 
 
-
-
-
-
-
-//2.same tree
-// Given two binary trees, write a function to check if they are the same or not.
-// Two binary trees are considered the same if they are structurally identical and the nodes have the same value
-
-
-//define a function that takes two parameters 
-//check if root node is same 
-//check if each of their nodes are same . if either of them is null or no match ,return false 
-//run recursive function until one of their node either gets null or no match 
-//return true if first tree's left and right nodes are exactly same as second 
-
-

d895551bce2b9e48d920e5b9e70e5e8c29ae08f4
Binay bk <khadkabinay@yahoo.com>
2021-01-02 00:03:40 -0600

    Adds smallerNumbersThanCurrent solution

--- index.js
+++ index.js
@@ -408,3 +408,36 @@ const  addDigits = function(num) {
 };
 
 addDigits(38)
+
+
+//16. How Many Numbers Are Smaller Than the Current Number
+//Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].Return the answer in an array.
+
+const smallerNumbersThanCurrent = function(nums) {
+    let newArr = []
+    let count   = 0; 
+for(let i = 0 ; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] > nums[j]){
+
+            count += 1
+                
+            }else{
+            continue
+            }
+            
+        }
+    if(count > 0){
+    newArr.push(count)
+    }else{
+    newArr.push(0)
+    }
+    
+    count = 0 
+    
+    
+}
+
+return newArr
+
+};

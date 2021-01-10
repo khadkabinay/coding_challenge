06ea0385bbe93bd097ec18ce9a32d1cef0273684
Binay bk <khadkabinay@yahoo.com>
2021-01-10 11:58:50 -0600

    Adds climbing stairs's solution

--- index.js
+++ index.js
@@ -535,3 +535,16 @@ const sumOddLengthSubarrays = function(arr) {
 };
 
 sumOddLengthSubarrays([1,4,2,5,3])
+
+
+//21. You are climbing a staircase. It takes n steps to reach the top.
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
+
+const climbStairs = function(n, memo = {1:1, 2:2}) {
+    if (memo[n] !== undefined) return memo[n];
+    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+    return memo[n];
+ };
+
+
+ climbStairs(30)

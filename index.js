725b1da9328a60ae2b19b24610e03d86e6182834
Binay bk <khadkabinay@yahoo.com>
2021-01-21 23:47:52 -0600

    Adds two string ewuivalent solution

--- index.js
+++ index.js
@@ -609,7 +609,7 @@ const maxProduct = function(nums) {
 maxProduct([3,4,5,2])
 
 
-//Height Checker
+// 25. Height Checker
 // Students are asked to stand in non-decreasing order of heights for an annual photo.Return the minimum number of  students that must move in order for all students to be standing in non-decreasing order of height . Notice that when a group of students is selected they can reorder in any possible way between themselves and the non selected students remain on their seats . 
 
 // Input: heights = [1,1,4,2,1,3]
@@ -631,7 +631,7 @@ const heightChecker = function(heights){
 }
 
 
-//Capitalize the first letter of words 
+// 26. Capitalize the first letter of words 
 
 function capitalizewords(array){
     let newArr = []
@@ -643,3 +643,22 @@ function capitalizewords(array){
 }
 
 console.log(capitalizewords(['hello', 'bye']))
+
+
+// 27.Check if two string Arrays are equivalent
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string and false otherwise. A string is represented by an array if the array elements concatenated in order forms the string . 
+
+const  arrayStringsAreEqual = function(word1, word2) {
+        let firstWord = word1.join("")
+        let secondWord = word2.join("")
+        if(firstWord === secondWord){
+            return true
+        }else{
+            return false
+        }
+    
+    
+    
+};
+
+arrayStringsAreEqual(['ab', 'c'], ['a', 'bc'])

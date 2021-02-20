d67bb2689a5a3b67707f25d8682e06ddb439f795
Binay bk <khadkabinay@yahoo.com>
2021-02-20 16:56:27 -0600

    Adds minSubArrayLen's solution

--- index.js
+++ index.js
@@ -749,3 +749,43 @@ var lengthOfLongestSubstring = function(s) {
 };
 
 lengthOfLongestSubstring('abcaabcd')
+
+
+//Sliding Window  - minSubArrayLen
+// write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
+// this function show return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function . If there is not one , return 0 instead.
+
+function minSubArrayLen(arr, num){
+ 
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+    let total = 0;
+    
+    while(start < arr.length){
+      
+       if(total < num && end < arr.length ){
+           total += arr[end]
+           end++
+       }else if (total >= num){
+          minLen = Math.min(minLen, end - start)
+          total -= arr[start]
+          start++
+          
+       }else{
+           break;
+       }
+       
+       
+        
+    }
+    
+    
+    return minLen === Infinity ? 0 : minLen;
+     
+     
+  }
+
+  minSubArrayLen([1,3,4,5,6], 11)//return 2
+
+
